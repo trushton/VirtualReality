d21f83b596c6f9583f548c78a753a088b7982036
Nolan Burfield <nburf@nevada.unr.edu>
2016-02-16 17:17:03 -0800

    updated the build remove

--- build/PA1/CMakeFiles/PA1_SUCCESSFUL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/PA1_SUCCESSFUL"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/PA1_SUCCESSFUL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
